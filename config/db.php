c1538b56df4070ebabbe2bd3f33879d1c9d86808
Ken Macharia <mkwaweru@gmail.com>
2017-07-17 16:23:06 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mmw',
+    'dsn' => 'mysql:host=localhost;dbname=dbname',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
